6be5888aac052f1e3a58222d8f2a675d33f1ea4c
Jeevan <31155545+jeevan0920@users.noreply.github.com>
2024-01-11 16:30:00 +0530

    first commit

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use rdkafka::producer::{FutureProducer, FutureRecord};
+use rdkafka::util::Timeout;
+
+#[tokio::main]
+async fn main() {
+    // Define Kafka producer configuration
+    let producer: FutureProducer = rdkafka::config::ClientConfig::new()
+        .set("bootstrap.servers", "localhost:9092")
+        .create()
+        .expect("Producer creation error");
+
+    // Define the Kafka topic to produce messages to
+    let kafka_topic = "test_kunjesh";
+
+    // Sample data to be written to Kafka
+    let sample_data = "Hello, Kunjesh!";
+
+    // Produce a message to the Kafka topic
+    let record = FutureRecord::to(kafka_topic)
+        .key("kunjesh_key")
+        .payload(sample_data);
+
+    // Use `Timeout::Never` to retry sending forver incase of failure
+    let timeout = Timeout::Never;
+
+    match producer.send(record, timeout).await {
+        Ok((partition, offset)) => {
+            println!(
+                "Message sent successfully: partition={}, offset={}",
+                partition, offset
+            )
+        }
+        Err((err, _)) => eprintln!("Error sending message: {:?}", err),
+    }
+}
